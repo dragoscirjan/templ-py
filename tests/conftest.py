a8ca2697743c58b9107ca6af0a5db1b32d42a8e5
Dragos Cirjan <dragos.cirjan@gmail+github2.ro>
2019-07-18 14:50:49 +0300

    testing precommit commmand on myself

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,7 +8,6 @@ from cement import fs
 
 @pytest.fixture(scope="function")
 def tmp(request):
-
 """
 Create a `tmp` object that geneates a unique temporary directory, and file
 for each test function that requires it.
