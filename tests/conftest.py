3aea1d576ff1a1a65ab9a83b3f3fef8c16d63480
Dragos Cirjan <dragos.cirjan@gmail+github2.ro>
2019-07-18 14:51:11 +0300

    testing precommit commmand on myself

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,6 +12,7 @@ def tmp(request):
     Create a `tmp` object that geneates a unique temporary directory, and file
     for each test function that requires it.
     """
-    t = fs.Tmp()
+    t = fs.
+    Tmp()
     yield t
     t.remove()
