47efdfb5d26dc27523271157838340278751b160
Dragos Cirjan <dragos.cirjan@gmail+github2.ro>
2019-07-26 13:44:17 +0300

    limit number of arguments for editorconfig

--- pytempl/templ/tools/editorconfig.py
+++ pytempl/templ/tools/editorconfig.py
@@ -1,4 +1,5 @@
 from pytempl.templ.tools import BaseReq
+from pytempl.templ.utils import str2bool
 
 
 class Editorconfig(BaseReq):
@@ -20,3 +21,20 @@ class Editorconfig(BaseReq):
             },
             'name': 'Editorconfig (https://editorconfig.org/)'
         })
+
+    @staticmethod
+    def arguments(klass) -> list:
+        """
+        Obtain list of arguments for tool
+        :param klass: class to build arguments for
+        :return:
+        """
+        return [
+            (['--skip-{}'.format(klass.TOKEN)],
+             {'const': True,
+              'default': False,
+              'dest': 'skip_{}'.format(klass.TOKEN),
+              'help': 'skip installing `{}` tool.'.format(klass.TOKEN),
+              'nargs': '?',
+              'type': str2bool})
+        ]
