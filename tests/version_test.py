4b4ef0315fcfe5b86d6f1533df16fc0b79bd390d
Dragos Cirjan <dragos.cirjan@itmediaconnect.ro>
2020-06-19 23:22:45 +0300

    +

---
+++ tests/version_test.py
@@ -0,0 +1,23 @@
+
+from python_template.version import get_static_version
+
+###
+# Model for using Pytest
+###
+
+# import pytest
+
+# def test_hello():
+#   assert get_static_version() == '0.0.1'
+
+
+###
+# Model for using UnitTest
+###
+
+import unittest
+
+class VersionTestCase(unittest.TestCase):
+
+  def test_get_static_version(self):
+    self.assertEqual(get_static_version(), '0.0.1')
